b0048658a50b42f170f6132184cba7a2b006d68e
Himanshu Pawar <you@example.com>
2018-07-26 13:58:55 +0530

    Updates

---
+++ app/src/main/java/com/example/himanshu/showbox/ViewAllMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.example.himanshu.showbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAllMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_movies);
+    }
+}
